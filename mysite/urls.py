15ca54d1ce9b352518b70281f902f6d017ec0aee
autumn <autumn.chiu.136@gmail.com>
2023-05-05 09:46:08 -0400

    p8: add third-party package, also base.html. (done woo)

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', lambda request: redirect('polls/')),
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
